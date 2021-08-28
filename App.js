bcb334523ed5659bcac37944cb885ef16e6601fc
smitethee <ervicjohn.vesencio@gmail.com>
2021-08-28 14:32:50 +0800

    added new title

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>ur tasks:</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'aquamarine',
   },
+  title:{
+    fontSize: '30px',
+    margin: '10px',
+  }
 });
